870ec8a233b283aa27703af1cfaf72bb0600d461
DEGRUYTER\towhid.islam <towhid.islam@degruyterbrill.com>
2025-10-11 14:27:27 +0200

    chore: updated

--- src/test/resources/testDB.sql
+++ src/test/resources/testDB.sql
@@ -3,15 +3,15 @@ DROP table IF EXISTS Passenger;
 DROP table IF EXISTS Person;
 
 CREATE TABLE IF NOT EXISTS Passenger
- (
- id int auto_increment primary key,
- name varchar(1024) null,
- age  int default '18' null,
- sex varchar(12) null,
- dob datetime null,
- createdate timestamp null,
- constraint Passenger_id_uindex unique (id)
- );
+(
+  id int auto_increment primary key,
+  name varchar(1024) null,
+  age  int default '18' null,
+  sex varchar(12) null,
+  dob datetime null,
+  createdate timestamp null,
+  constraint Passenger_id_uindex unique (id)
+);
 
 CREATE TABLE IF NOT EXISTS Person
 (
