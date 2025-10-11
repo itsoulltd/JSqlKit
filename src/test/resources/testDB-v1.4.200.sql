4d36e5efdfbaa3fc2c9fdd406fca740d10e18ddd
DEGRUYTER\towhid.islam <towhid.islam@degruyterbrill.com>
2025-10-11 14:55:43 +0200

    sql: static file update

--- src/test/resources/testDB-v1.4.200.sql
+++ src/test/resources/testDB-v1.4.200.sql
@@ -1,24 +1,24 @@
 CREATE TABLE IF NOT EXISTS Passenger
 (
-  id         int auto_increment primary key,
-  name       varchar(1024) null,
-  age        int default '18' null,
-  sex        varchar(12) null,
-  dob        datetime null,
-  active     tinyint(1) default '0',
-  constraint Passenger_id_uindex unique (id)
+    id         int auto_increment primary key,
+    name       varchar(1024) null,
+    age        int default '18' null,
+    sex        varchar(12) null,
+    dob        datetime null,
+    createdate timestamp null,
+    constraint Passenger_id_uindex unique (id)
 );
 
 CREATE TABLE IF NOT EXISTS Person
 (
-  uuid       varchar(512) primary key,
-  name       varchar(512) null,
-  age        int          null,
-  gender     varchar(12)  null,
-  email      varchar(512) null,
-  active     tinyint(1)   null,
-  salary     double       null,
-  dob        datetime     null,
-  height     float        null,
-  createDate timestamp    null
+    uuid       varchar(512) not null primary key,
+    name       varchar(512) null,
+    age        int          null,
+    active     tinyint(1)   null,
+    salary     double       null,
+    dob        datetime     null,
+    height     float        null,
+    createDate timestamp    null,
+    dobDate    datetime     null,
+    createTime timestamp    null
 );
--- src/test/resources/testDB-v2.2.220.sql
+++ src/test/resources/testDB-v2.2.220.sql
@@ -1,23 +1,29 @@
 CREATE TABLE IF NOT EXISTS Passenger
 (
-  id         BIGINT PRIMARY KEY,
-  name       VARCHAR(1024) NOT NULL,
-  age        INT CHECK (age > 0),
-  sex        VARCHAR(12) NULL,
-  dob        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  active     BOOLEAN DEFAULT FALSE
+    id         BIGINT PRIMARY KEY,
+    name       VARCHAR(1024) NOT NULL,
+    age        INT CHECK (age > 0),
+    sex        VARCHAR(12) NULL,
+    dob        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    createdate TIMESTAMP NULL,
 );
 
 CREATE TABLE IF NOT EXISTS Person
 (
-  uuid       VARCHAR(512) PRIMARY KEY,
-  name       VARCHAR(512) NULL,
-  age        INT CHECK (age > 0),
-  gender     VARCHAR(12) NULL,
-  email      VARCHAR(255) UNIQUE,
-  active     BOOLEAN DEFAULT FALSE,
-  salary     DECIMAL(10, 2) NULL,
-  dob        TIMESTAMP     NULL,
-  height     DECIMAL(10, 2) NULL,
-  createDate TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+    uuid       VARCHAR(512) PRIMARY KEY,
+    name       VARCHAR(512) NULL,
+    age        INT CHECK (age > 0),
+    active     BOOLEAN DEFAULT FALSE,
+    salary     DECIMAL(10, 2) NULL,
+    dob        TIMESTAMP     NULL,
+    height     DECIMAL(10, 2) NULL,
+    createDate TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+    dobDate    TIMESTAMP    NULL,
+    createTime TIME    NULL
 );
+
+--Example:
+--------------------------
+--gender     VARCHAR(12) NULL,
+--email      VARCHAR(255) UNIQUE NOT NULL,
+---------------------------
